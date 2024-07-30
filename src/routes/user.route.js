151faf79cf9b8837ba37940e702f25272d10c254
arongsnuna <unhi000@naver.com>
2024-07-30 14:09:42 -0700

    Style: Endpoint 수정

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,6 @@ const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.post('/register', UserController.registerUser);
+router.post('/signup', UserController.registerUser);
 
 module.exports = router;
