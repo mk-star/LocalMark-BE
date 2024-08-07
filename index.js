f170235d5cced5556cd15e7e43acf6c75a011841
mk-star <ssunn0812@naver.com>
2024-08-08 00:23:31 +0900

    Fix: PR Conflict 해결(#44)

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import specs from "./config/swagger.config.js"; 
+import specs from "./config/swagger.config.js";
 import cookieParser from "cookie-parser";
 import { response } from './config/response.js';
+import { status } from './config/response.status.js';
 import { postRouter } from "./src/routes/post.route.js";
 import { authRouter } from "./src/routes/auth.route.js"; // .js 확장자 추가
 import { likeRouter } from "./src/routes/Like.route.js";
 import { commentRouter } from "./src/routes/comment.route.js";
+import { healthRoute } from "./src/routes/health.route.js";
+import { morelocalRouter } from './src/routes/morelocal.routes.js';
+import { brandRouter } from "./src/routes/brand.route.js"
 import { reviewRouter } from "./src/routes/review.route.js";
 
 //서버 가동
 dotenv.config();
 const app = express();
 
-// server setting
-app.set("port", process.env.PORT || 3000); // 서버 포트 지정
-app.use(cors()); // cors 방식 허용
-app.use(express.static("public")); // 정적 파일 접근
-app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함
+// 서버 설정
+app.set("port", process.env.PORT || 3000);
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // swagger
@@ -31,20 +35,25 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 app.use("/likes", likeRouter);
 app.use("/auth", authRouter);
+app.use('/brand', brandRouter);
+app.use('/morelocal', morelocalRouter);
 app.use("/reviews", reviewRouter);
+
+app.use("/health", healthRoute);
 
 app.get("/", (req, res) => {
   res.send("로컬마크 시작~");
 });
 
 app.use((err, req, res, next) => {
-  // 템플릿 엔진 변수 설정
-  res.locals.message = err.message;   
-  // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
-  console.error(err);
-  res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    console.error(err);
+    res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
+
 app.listen(app.get('port'), () => {
-  console.log(`Example app listening on port ${app.get('port')}`);
+    console.log(`Example app listening on port ${app.get('port')}`);
 });
