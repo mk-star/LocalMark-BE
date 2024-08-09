52fc5411cbf5982c3d4b843bd0dd01aed720f26b
arongsnuna <unhi000@naver.com>
2024-08-09 02:16:55 -0700

    Fix: loginrouter 분리

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,21 +1,8 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 import { registerGeneral, registerCreator, findUsername, getOrderItems, updateUser, updatePassword, updatePasswordEmail } from "../controllers/user.controller.js";
-import { login, logout } from "../controllers/auth.controller.js";
-
 import { jwtMiddleware } from "../../config/userJwtMiddleWare.js";
 
-export const loginRouter = express.Router();
-export const logoutRouter = express.Router();
 export const userRouter = express.Router();
-
-loginRouter.post("", asyncHandler(login));
-
-loginRouter.get("/profile", jwtMiddleware, (req, res) => {
-  res.json({ message: `로그인한 유저의 아이디: ${req.currentId}` });
-});
-
-logoutRouter.post("", asyncHandler(logout));
 
 userRouter.post('/signup/creator', registerCreator);
 userRouter.post('/signup/general', registerGeneral);
