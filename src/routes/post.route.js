04232f78cb61f414c3fb3faefe7bd5121759f149
dokyung-kang <kangdk02@naver.com>
2024-08-10 14:47:13 +0900

    feat: post 임시 주석 처리(#63)

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,17 +1,17 @@
-const upload = multer({ storage });
-const uploadMiddleware = upload.array("images",10);
+// const upload = multer({ storage });
+// const uploadMiddleware = upload.array("images",10);
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { addPost } from '../controllers/post.controller';
-import { modifyPost, postDetail, posts, removePost, uploadPost } from '../controllers/post.controller';
-import { upload } from '../proviers/image.provider';
+// import { addPost } from '../controllers/post.controller';
+// import { modifyPost, postDetail, posts, removePost, uploadPost } from '../controllers/post.controller';
+// import { upload } from '../proviers/image.provider';
 
 export const postRouter = express.Router({mergeParams: true});
 
-postRouter.post('/signin', asyncHandler(addPost));
-postsRouter.get('/', asyncHandler(posts));
-postsRouter.post('/', upload.array('images', 10), asyncHandler(uploadPost));
-postsRouter.get('/:postId', asyncHandler(postDetail));
-postsRouter.patch('/:postId', upload.array('images', 10), asyncHandler(modifyPost));
-postsRouter.delete('/:postId', asyncHandler(removePost));
+// postRouter.post('/signin', asyncHandler(addPost));
+// postsRouter.get('/', asyncHandler(posts));
+// postsRouter.post('/', upload.array('images', 10), asyncHandler(uploadPost));
+// postsRouter.get('/:postId', asyncHandler(postDetail));
+// postsRouter.patch('/:postId', upload.array('images', 10), asyncHandler(modifyPost));
+// postsRouter.delete('/:postId', asyncHandler(removePost));
 
