d8a030bcade78d655fc6c9a4f7b3fbe12f490f96
mk-star <ssunn0812@naver.com>
2024-08-11 14:44:01 +0900

    Fix: posts 메소드 중복 선언 해결(#62)

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -7,20 +7,6 @@ import {
         getPostsByCreatorService,  
         modifyPostDetail } from "../services/post.service.js";
         
-// 커뮤니티 게시글 전체 및 카테고리별 게시글 목록 조회
-export const posts = async(req, res) => {
-
-        const querys = req.query;
-        console.log(querys);
-
-        const category = querys.category;
-        console.log(category);
-        const page = parseInt(querys.page);
-
-        res.send(response(status.SUCCESS, await getPosts(category, page)));
-
-}
-
 export const addPost = async(req, res) => {
 
         console.log("body: ", req.body);
