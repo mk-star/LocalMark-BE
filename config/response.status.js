a419764c4c58c547d435f8530a25b3a1fb2d0e61
mk-star <ssunn0812@naver.com>
2024-08-11 15:25:58 +0900

    Refactor: 인증되지 않은 토큰에 대해 에러 응답 반환(#15)

--- config/response.status.js
+++ config/response.status.js
@@ -127,10 +127,10 @@ export const status = {
     message: "토큰이 존재하지 않습니다.",
   },
   TOKEN_UNAUTHORIZED: {
-    status: StatusCodes.BAD_REQUEST,
+    status: StatusCodes.UNAUTHORIZED,
     isSuccess: false,
     code: "USER4007",
-    message: "유효하지 않은 토큰입니다.",
+    message: "유효하지 않은 로그인 토큰입니다.",
   },
   FORBIDDEN_APPOROACH: {
     status: StatusCodes.FORBIDDEN,
