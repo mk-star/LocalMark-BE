c51c574a7e539e362788dd17ab8ebc13b340cbf4
arongsnuna <unhi000@naver.com>
2024-08-19 15:13:42 +0900

    Feat: 브랜드 이미지 s3

--- src/routes/brand.route.js
+++ src/routes/brand.route.js
@@ -1,14 +1,21 @@
 import express from "express";
 import asyncHandler from 'express-async-handler';
 import { jwtMiddleware } from "../../config/userJwtMiddleWare.js";
+import { imageUploader } from "../middleware/image.uploader.js";
 
 import { brandInfo, brandProductList, brandOrderList } from "../controllers/brand.controller.js";
 import { createBrand, updateBrand } from '../controllers/brand.controller.js';
 
-export const brandRouter = express.Router();
+export const brandRouter = express.Router({mergeParams: true});
 
-brandRouter.post('/', jwtMiddleware, asyncHandler(createBrand));
-brandRouter.patch('/:brandId', jwtMiddleware, asyncHandler(updateBrand));
+brandRouter.post('/',
+    jwtMiddleware,
+    imageUploader.single('brand_image'),
+    asyncHandler(createBrand));
+brandRouter.patch('/:brandId',
+    jwtMiddleware,
+    imageUploader.single('brand_image'),
+    asyncHandler(updateBrand));
 
 brandRouter.get('/:brandId', asyncHandler(brandInfo));
 brandRouter.get('/:brandId/products', asyncHandler(brandProductList));
