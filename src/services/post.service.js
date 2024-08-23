5dcac3bb0872328e6fad4e5c6dcac5d1833e4cb0
mk-star <ssunn0812@naver.com>
2024-08-23 11:24:35 +0900

    feat: 게시글 작성 시 사진 url 수정 (#61)

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -15,16 +15,19 @@ export const addPostInfo = async (userId, body, imagekeys) => {
 
         try {
             let thumbnail_filename = null;
+            let s3Url = null;
 
             if (imagekeys[0]) {
                 thumbnail_filename = encodeURIComponent(imagekeys[0]);
+                s3Url = `https://localmark-s3.s3.ap-northeast-2.amazonaws.com/${thumbnail_filename}`;
             }
+
             const postId = await addPost({
                 "userId": userId,
                 "title": body.title,
                 "content": body.content,
                 "category": body.category,
-                "thumnail_filename": thumbnail_filename, //null 이 될 수 있음. (이미지 첨부를 아무것도 하지 않았을 때)
+                "thumnail_filename": s3Url, //null 이 될 수 있음. (이미지 첨부를 아무것도 하지 않았을 때)
             })
 
             console.log(imagekeys);
